81f91efbbbfa0132cddbc672573b2738afe45bfd
HARY JOHNS <haryjohns@hotmail.com>
2026-06-25 04:47:57 +0530

    Create check_imunify.sh

---
+++ scripts/check_imunify.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if systemctl is-active --quiet imunify360; then
+echo "OK - Imunify360 is running"
+exit 0
+elif systemctl is-active --quiet imunify-antivirus; then
+echo "OK - ImunifyAV is running"
+exit 0
+else
+users=$(w)
+logins=$(last | head -n 10)
+
+```
+echo -e "CRITICAL - Neither Imunify360 nor ImunifyAV is running\n\nCurrent logged users:\n$users\n\nLast logged users:\n$logins"
+exit 2
+```
+
+fi
