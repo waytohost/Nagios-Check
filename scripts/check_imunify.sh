06f01ae181d39bfcd47017ef52081f24949a9707
HARY JOHNS <haryjohns@hotmail.com>
2026-06-25 05:38:49 +0530

    Update check_imunify.sh

--- scripts/check_imunify.sh
+++ scripts/check_imunify.sh
@@ -11,8 +11,18 @@ users=$(w)
 logins=$(last | head -n 10)
 
 
-echo -e "CRITICAL - Neither Imunify360 nor ImunifyAV is running\n\nCurrent logged users:\n$users\n\nLast logged users:\n$logins"
+#echo -e "CRITICAL - Neither Imunify360 nor ImunifyAV is running\n\nCurrent logged users:\n$users\n\nLast logged users:\n$logins"
+echo "CRITICAL - Neither Imunify360 nor ImunifyAV is running"
+    echo
+    echo "Current logged users:"
+    echo "$users"
+    echo
+    echo "Last logged users:"
+    echo "$logins"
+
 exit 2
+
+
 
 
 fi
